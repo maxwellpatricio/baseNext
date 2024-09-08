fa2a689c5323481fde0d0c612a1a318b91f25608
Maxwell <maxwellpatri@gmail.com>
2024-09-08 12:51:59 -0300

    Simplificação do Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -11,34 +11,22 @@ type CardType = {
 
 export const Card = ({
     title = '',
-    date = 2,
     imgUrl = ''
 }: CardType) => {
     return (
         <S.CardWrapper>
-            <S.CardImage background={imgUrl} />
+            {/* <S.CardImage background={imgUrl} /> */}
             <S.CardTextWrapper>
-                <S.CardTextDate>{date} days ago</S.CardTextDate>
                 <S.CardTextTitle>{title}</S.CardTextTitle>
-                <S.CardTextBody>
-                    Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae
-                    temporibus omnis illum maxime quod deserunt eligendi dolor
-                </S.CardTextBody>
             </S.CardTextWrapper>
-            <S.CardStatWrapper>
-                {/* <CardStats>
-              <div>
-                1<sup>m</sup>
-              </div>
-              <div>read</div>
-            </CardStats> */}
+            <S.CardFooterWrapper>
                 <S.CardStats>
-                    <S.LinkText href="#">website</S.LinkText>
+                    <S.LinkText href="#">Ver mais</S.LinkText>
                 </S.CardStats>
                 <S.CardStats>
-                    <S.LinkText href="#">github</S.LinkText>
+                    <S.LinkText href="#">Adicionar</S.LinkText>
                 </S.CardStats>
-            </S.CardStatWrapper>
+            </S.CardFooterWrapper>
         </S.CardWrapper>
     );
 };
--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,13 +1,14 @@
 import styled from "styled-components";
+import { theme } from '@/styles/theme';
 
 export const CardWrapper = styled.div`
   display: grid;
   grid-template-columns: 300px;
-  grid-template-rows: 210px 210px 80px;
+  grid-template-rows: 20px 80px 80px;
   grid-template-areas: "image" "text" "stats";
   border-radius: 18px;
-  background: #000;
-  box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.9);
+  background: ${theme.colors.primary};
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.4);
   text-align: center;
 `;
 
@@ -36,16 +37,7 @@ export const CardTextTitle = styled.h2`
   min-width: 0px;
   line-height: 1.2;
   margin: 0px;
-  background: linear-gradient(
-    110.78deg,
-    rgb(118, 230, 80) -1.13%,
-    rgb(249, 214, 73) 15.22%,
-    rgb(240, 142, 53) 32.09%,
-    rgb(236, 81, 87) 48.96%,
-    rgb(255, 24, 189) 67.94%,
-    rgb(26, 75, 255) 85.34%,
-    rgb(98, 216, 249) 99.57%
-  );
+  background: #000;
   background-clip: text;
   -webkit-background-clip: text;
   color: transparent;
@@ -57,7 +49,7 @@ export const CardTextBody = styled.p`
   font-weight: 300;
 `;
 
-export const CardStatWrapper = styled.div`
+export const CardFooterWrapper = styled.div`
   grid-area: stats;
   display: grid;
   /* grid-template-columns: 1fr 1fr 1fr; */
@@ -66,7 +58,7 @@ export const CardStatWrapper = styled.div`
 
   border-bottom-left-radius: 15px;
   border-bottom-right-radius: 15px;
-  background: #5930e5;
+  background:  ${theme.colors.thirdary};;
 `;
 
 export const CardStats = styled.div`
